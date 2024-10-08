ec3a88579d87945edbf3eb03bbc3edcb636f9005
noflnofl <phurin.byp@gmail.com>
2024-10-08 22:10:15 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import smbus2
+import time
+
+app = Flask(__name__)
+
+# Change to IP address of the Raspberry Pi
+PI_HOST = '0.0.0.0'
+
+bus = smbus2.SMBus(1)
+
+ARDUINO_ADDRESS = 0x08
+
+def send_data(data):
+  	with smbus2.SMBus(1) as bus:
+  		bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, [ord(c) for c in data])
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/send_command/<command>')
+def handle_command(command):
+	send_data(command)
+	return f"Command '{command}' sent to Arduino."
+
+if __name__ == '__main__':
+  	app.run(debug=True, host=PI_HOST, port=5000)
