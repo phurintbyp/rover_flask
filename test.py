edb4ba3fddd1c950867bbe8da314fde54c7f29fc
noflnofl <phurin.byp@gmail.com>
2024-10-10 00:55:16 +0700

    changed test

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 import time
 
 picam2 = Picamera2()
 
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
+# Configure the camera
+picam2.configure(picam2.create_preview_configuration())
 
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview()
 picam2.start()
 
+# Give the camera some time to initialize
 time.sleep(2)
 
-picam2.capture_file("test_photo.jpg")
+# Capture a single frame
+frame = picam2.capture_array()
+print("Captured frame successfully!")
+
+picam2.stop()
