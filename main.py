1b0cba14a9b8aeff3a921ef4ff605498f94fbc6b
noflnofl <phurin.byp@gmail.com>
2024-10-09 22:55:30 +0700

    only camera

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ app = Flask(__name__)
 # Change to IP address of the Raspberry Pi
 PI_HOST = '0.0.0.0'
 
-bus = smbus2.SMBus(1)
-ARDUINO_ADDRESS = 0x08  # I2C address of Arduino
+# bus = smbus2.SMBus(1)
+# ARDUINO_ADDRESS = 0x08
 camera = Camera()  # Create a Camera object
 
-def send_data(data):
-    with smbus2.SMBus(1) as bus:
-        bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, [ord(c) for c in data])
+# def send_data(data):
+#     with smbus2.SMBus(1) as bus:
+#         bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, [ord(c) for c in data])
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/send_command/<command>')
-def handle_command(command):
-    send_data(command)
-    return f"Command '{command}' sent to Arduino."
+# @app.route('/send_command/<command>')
+# def handle_command(command):
+#     send_data(command)
+#     return f"Command '{command}' sent to Arduino."
 
 @app.route('/video_feed')
 def video_feed():
