73e0e243c2d186d2788d67b03d8c709f7be25ee8
noflnofl <phurin.byp@gmail.com>
2024-10-10 21:30:35 +0700

    seperate to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,3 @@
 function sendCommand(command) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/send_command/' + command, true);
-    xhr.send();
+    fetch(`/send_command/${command}`);
 }
-
-function displayWebcamFeed() {
-    const webcamFeedUrl = 'http://192.168.0.152:8080/stream';
-    const webcamFeedDiv = document.getElementById('camera-feed');
-    const webcamImage = document.createElement('img');
-    webcamImage.src = webcamFeedUrl;
-    webcamImage.alt = 'Camera Feed';
-    webcamFeedDiv.appendChild(webcamImage);
-}
-
-window.onload = function() {
-    displayWebcamFeed();
-};
