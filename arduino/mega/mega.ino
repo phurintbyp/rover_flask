4d88a71e6767faeb83bca391eab5bd2ac9c71883
noflnofl <phurin.byp@gmail.com>
2024-10-14 20:52:18 +0700

    test map

--- arduino/mega/mega.ino
+++ arduino/mega/mega.ino
@@ -11,6 +11,8 @@ void setup() {
   Wire.begin(SLAVE_ADDRESS);  // Join I2C bus as a slave with address 0x08
   Wire.onReceive(receiveData);  // Register function to receive I2C data
   pinMode(13, OUTPUT);
+  myServo.attach(4);
+  myServo.write(90);
   Serial.begin(9600);  // Initialize serial communication for debugging
   Serial1.begin(9600);
 }
@@ -22,10 +24,16 @@ void loop() {
 void receiveData(int byteCount) {
   while (Wire.available()) {
     command = Wire.read();  // Read the incoming I2C data
-    if (command == 'i') {
-      digitalWrite(13, HIGH);
-    } else if (command == 'd') {
-      digitalWrite(13, LOW);
+    if (command == 'w') {
+      myServo.write(0);
+    } else if (command == 'a') {
+      myServo.write(180);
+    }else if (command == 's') {
+      myServo.write(180);
+    }else if (command == 'd') {
+      myServo.write(180);
+    }else if (command == 'h') {
+      myServo.write(180);
     }
   }
 }
