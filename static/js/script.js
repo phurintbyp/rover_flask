f10dc6751236e0698fe75e8aae4f6889ab25efa4
noflnofl <phurin.byp@gmail.com>
2024-10-16 23:24:00 +0700

    fix

--- static/js/script.js
+++ static/js/script.js
@@ -145,7 +145,6 @@ var drawControl = new L.Control.Draw({
         remove: true
     }
 });
-map.addControl(drawControl);
 
 // Function to toggle functionality based on drawPath value
 function toggleDrawPath() {
@@ -351,11 +350,14 @@ function disableClickControl() {
     map.off('click'); // Disable the click event listener
 }
 
+// Initially enable route finding
+enableInputs();
+
 function enableInputs() {
     document.getElementById('quality').disabled = false;
     document.getElementById('getValueButton').disabled = false;
     document.getElementById('generateButton').disabled = false;
-    map.addControl(drawControl);
+    map.removeControl(drawControl);
 }
 
 // Function to disable the Quality input and Get Value button
@@ -363,5 +365,5 @@ function disableInputs() {
     document.getElementById('quality').disabled = true;
     document.getElementById('getValueButton').disabled = true;
     document.getElementById('generateButton').disabled = true;
-    map.removeControl(drawControl);
+    map.addControl(drawControl);
 }
